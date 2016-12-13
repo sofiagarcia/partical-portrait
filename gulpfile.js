c2c31d6896f4db29facad66a7566439cc4afb3d7
Sofia Garcia <sofiagarcia@sofias-air-2.attlocal.net>
2016-12-13 12:26:57 -0500

    setup dev server to update every time js or html file changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) //using gulp-sass
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 })
 
-gulp.task('watch', function() {
+gulp.task('watch',['browserSync', 'sass'], function() {
   gulp.watch('app/scss/**.*.scss', ['sass']);
+  //reload broswer when HTML or JS files change
+  gulp.watch('app/*.html', broswerSync.reload);
+  gulp.watch('app/js/**/*.js', broswerSync.reload);
+});
 
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
