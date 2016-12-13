0ba12180fb20489a14cbc407fd5f06a343cea966
Sofia Garcia <sofiagarcia@sofias-air-2.attlocal.net>
2016-12-13 11:56:15 -0500

    initial commit

---
+++ app/js/sketch.js
@@ -0,0 +1,13 @@
+//Emotion Photo Filter by Sofia Garcia
+// v.1 Dec. 2016
+
+var myCanvas;
+
+function setup() {
+  myCanvas = createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+}
